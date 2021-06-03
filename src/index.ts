e6e10ff8918ed946c62a41e2dd639e46a2c9ba9c
Jair Orlando Huaman Bellido <huamanbellidoj@gmail.com>
2021-06-03 15:48:30 -0700

    Add integration with dynamodb

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { DynamoDB } from "aws-sdk";
+import { v4 as uuidv4 } from "uuid";
+
+const dynamodb = new DynamoDB({
+  apiVersion: '2012-08-10',
+});
+
+const table = "suscriptions"
+
+const createSuscription = async (name: string) => {
+  return await dynamodb.putItem({
+    TableName: table,
+    Item: {
+      ["name"]: { S: name },
+      ["id"]: { N: uuidv4() }
+    },
+  }).promise()
+}
 
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Saludos desde el API Gateway",
-    }),
-  };
 
-  if (event.httpMethod === "GET") {
-    return response;
-  }
+  return await createSuscription("olvid")
+    .then(() => ({ statusCode: 200, body: JSON.stringify({ "correcta": "prueba" }) }))
+    .catch(e => ({ statusCode: 400, body: JSON.stringify({ "error": e }) }))
+
 };
+
+
+
